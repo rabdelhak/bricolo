c6935fb43ffd8d245e5d9379bc43a4cc8dc67e9c
abdelhak <78546172+rabdelhak@users.noreply.github.com>
2025-05-04 00:53:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,52 @@ hamburger.addEventListener("click", () => {
     dropdown.classList.toggle("show");
 });
 
-// Scroll to section
+/* Scroll to section
 function scrollToSection(id) {
     const section = document.getElementById(id);
     section.scrollIntoView({ behavior: "smooth" });
+}*/
+
+
+// Scroll vers les sections
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    const offset = 30; // Compense la navbar fixe
+    
+    window.scrollTo({
+        top: section.offsetTop - offset,
+        behavior: "smooth"
+    });
 }
 
+// Ferme le menu dropdown quand on clique sur un lien
+document.querySelectorAll(".navbar-dropdown a").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        dropdown.classList.remove("show");
+        document.body.style.overflow = "";
+    });
+});
 
 
+
+
+//btn cacher log
+ function openLoginModal() {
+    document.getElementById("loginModal").style.display = "flex";
+  }
+
+  function closeLoginModal() {
+    document.getElementById("loginModal").style.display = "none";
+  }
+
+  // Fermer la modale en cliquant en dehors
+  window.onclick = function(e) {
+    const modal = document.getElementById("loginModal");
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 
 
 
